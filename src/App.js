1291efce38be800b630f7f3353e17c4dbc2e2866
nadeeraka <nimanthanadeeraka@outlook.com>
2019-07-01 14:46:14 +0530

    stART

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+//import Logic from "./components/Logic";
 
 function App() {
   return (
     <div className="App">
-      <p>jjjj</p>
+      <h1 className="text-center mt-3">Todo app</h1>
     </div>
   );
 }
